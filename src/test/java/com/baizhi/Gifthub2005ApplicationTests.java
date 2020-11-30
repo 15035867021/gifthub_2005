18bf1f4f2d192f5e0ba099da7c71314f93751e8e
zhangxh <zhang_xh624@163.com>
2020-11-30 21:57:56 +0800

    gifthub第一次提交

---
+++ src/test/java/com/baizhi/Gifthub2005ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.baizhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gifthub2005ApplicationTests {
+
+    @Test
+    void contextLoads() {
+        int a = 100;
+        int b = 200;
+    }
+
+}
