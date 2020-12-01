5eff7153e18fe18f5a431415190596d33460aefc
zhangxh <zhang_xh624@163.com>
2020-12-01 18:56:14 +0800

    第一次测试类修改

--- src/test/java/com/baizhi/Gifthub2005ApplicationTests.java
+++ src/test/java/com/baizhi/Gifthub2005ApplicationTests.java
@@ -3,6 +3,10 @@ package com.baizhi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 @SpringBootTest
 class Gifthub2005ApplicationTests {
 
@@ -10,6 +14,12 @@ class Gifthub2005ApplicationTests {
     void contextLoads() {
         int a = 100;
         int b = 200;
+        HashMap<Object, Object> map = new HashMap<>();
+        Set<Map.Entry<Object, Object>> entries = map.entrySet();
+        for (Map.Entry<Object, Object> entry : entries) {
+            Object value = entry.getValue();
+            Object key = entry.getKey();
+        }
     }
 
 }
